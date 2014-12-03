6fdd379b64974073473af5148d73897fdd88af3a
lhutzelmann <lars_hutzelmann@gmx.de>
2014-12-03 16:51:53 +0100

    clean up and doc-strings

--- urldownloader/urldownloader.py
+++ urldownloader/urldownloader.py
@@ -6,18 +6,28 @@ import string
 import urllib2
 import sys
 
+DEFAULT_ENCODING = 'utf-8'
+
+
 class UrlDownloader(object):
     """ Class to download a file from a given URL.
     """
+    # Implemented as class for better extensibility.
     def __extract_filename(self, text_url):
-        """
+        """ Extracts the file name from the url.
+        Requires the URL to contain the file name as last segment.
+        Therefore no query parameters are allowed.
+
         :param text_url: URL as text string
         :return: the file name extracted from the URL
         """
         return text_url.split('?')[0].rsplit('/', 1)[1]
 
     def download(self, text_url):
-        """
+        """ Perform the download. Opens the URL, retrieves the response data
+        and saves it to a file using a file name extracted from the URL.
+        The file is saved to the current working directory.
+
         :param text_url: URL as text string
         """
         print u'Downloading %s' % text_url
@@ -32,23 +42,19 @@ class UrlDownloader(object):
         else:
             raise RuntimeError("Response status = %d" % stat)
 
-def main(encoding='utf-8'):
-    # Set up argument parser.
-    parser = argparse.ArgumentParser(
-        description='Download web content from text URLs in a file.')
-    parser.add_argument('file', metavar='FILE', nargs='+',
-                        help='a file which contains one URL per line')
-    parser.add_argument('-e', '--encoding', default=encoding,
-                        help=('set the encoding of the file (default=%s)' %
-                              encoding))
+def main(files, encoding=DEFAULT_ENCODING):
+    """ Main function of the script. Performs the actual work and can be called
+    from other code directly.
 
-    args = parser.parse_args()
-
+    :param files: Sequence of file paths to the files that contain the URLs.
+    :param encoding: Defines the encoding of the input files.
+    :return: None if everything went fine, else 1.
+    """
     url_downloader = UrlDownloader()
 
     exit_code = None
-    for file_path in args.file:
-        with codecs.open(file_path, encoding=args.encoding) as f:
+    for file_path in files:
+        with codecs.open(file_path, encoding=encoding) as f:
             for url in f:
                 url = string.strip(url)   # remove whitespace
                 try:
@@ -56,7 +62,20 @@ def main(encoding='utf-8'):
                 except Exception as e:
                     print "Error occurred: %s" % e
                     exit_code = 1
-    sys.exit(exit_code)
+    return exit_code
 
 if __name__ == '__main__':
-    main()
+    # Set up argument parser for command line handling.
+    parser = argparse.ArgumentParser(
+        description='Download web content from URLs in files.')
+    parser.add_argument('file', metavar='FILE', nargs='+', dest='files',
+                        help='file which contains one URL per line')
+    parser.add_argument('-e', '--encoding', default=DEFAULT_ENCODING,
+                        help=('set the encoding of the file (default=%s)' %
+                              DEFAULT_ENCODING))
+
+    # Get command line arguments as a dictionary.
+    kwargs = vars(parser.parse_args())
+
+    exit_code = main(**kwargs)
+    sys.exit(exit_code)
